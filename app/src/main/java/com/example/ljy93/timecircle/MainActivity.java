0c4bdf0cecbb667bbe5cae4e0248a38a35c96e7c
lozenia64 <lozenia64@gmail.com>
2018-10-02 19:38:30 +0900

    StartProject
    StartProject

---
+++ app/src/main/java/com/example/ljy93/timecircle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ljy93.timecircle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// 메인파일, 아직 아무것도 작성하지 않음.
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
