5e6f8c29928640069c35d103c67825fc2a7c77a4
lozenia64 <lozenia64@gmail.com>
2018-10-04 18:27:18 +0900

    2018.10.04.TapHost
    TapHost

---
+++ app/src/main/java/com/example/ljy93/timecircle/TabActivity_1.java
@@ -0,0 +1,15 @@
+package com.example.ljy93.timecircle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.ljy93.timecircle.R;
+
+public class TabActivity_1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_activity_1);
+    }
+}
---
+++ app/src/main/java/com/example/ljy93/timecircle/TabActivity_3.java
@@ -0,0 +1,17 @@
+package com.example.ljy93.timecircle;
+
+/**
+ * Created by sung woo cho on 2018-05-07.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TabActivity_3 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_activity_3);
+    }
+}
