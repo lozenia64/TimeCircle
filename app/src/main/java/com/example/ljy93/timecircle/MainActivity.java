cb5ac106861990e602f3c3601d9c9c478742bbfe
lozenia64 <lozenia64@gmail.com>
2018-10-22 18:13:10 +0900

    update

--- app/src/main/java/com/example/ljy93/timecircle/MainActivity.java
+++ app/src/main/java/com/example/ljy93/timecircle/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends TabActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         TabHostWindow = (TabHost)findViewById(android.R.id.tabhost);
 
         TabSpec TabMenu1 = TabHostWindow.newTabSpec("First tab");
