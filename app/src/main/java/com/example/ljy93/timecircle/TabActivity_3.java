d3302c3436e692d7c4be61d1088ff109e222e21f
lozenia64 <lozenia64@gmail.com>
2018-11-22 00:42:51 +0900

    Update TabActivity_3.java
    Integer.parseInt 예외처리

--- app/src/main/java/com/example/ljy93/timecircle/TabActivity_3.java
+++ app/src/main/java/com/example/ljy93/timecircle/TabActivity_3.java
@@ -19,9 +19,8 @@ import com.example.ljy93.timecircle.R;
 public class TabActivity_3 extends Activity {
     Button sbtn3;
     EditText dateY, dateM, dateD, time1, time2, name3;
-    RadioGroup radioColor2;
-    String touchedColor3;
     Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,18 +41,28 @@ public class TabActivity_3 extends Activity {
                 String startTime = time1.getText().toString();
                 String endTime = time2.getText().toString();
                 String name = name3.getText().toString();
-                String color = touchedColor3;
 
-                if (date == "" || startTime == "" || endTime == ""  || name == "" || color == "") {
-                    Toast.makeText(getApplicationContext(), "입력하지 않은 값이 존재합니다!", Toast.LENGTH_SHORT).show();
+                try{
+                    int intM = Integer.parseInt(dateM.getText().toString());
+                    int intD = Integer.parseInt(dateD.getText().toString());
+                    int Stime = Integer.parseInt(time1.getText().toString());
+                    int Etime = Integer.parseInt(time2.getText().toString());
+
+                    if (date == "" || startTime == "" || endTime == ""  || name == "") Toast.makeText(getApplicationContext(), "입력하지 않은 값이 존재합니다!", Toast.LENGTH_SHORT).show();
+                    else if (intM > 12 || intM < 1) Toast.makeText(getApplicationContext(), "월은 1~12사이의 값을 입력해주세요!", Toast.LENGTH_SHORT).show();
+                    else if (intD > 31 || intD < 1) Toast.makeText(getApplicationContext(), "일은 1~31사이의 값을 입력해주세요!", Toast.LENGTH_SHORT).show();
+                    else if (Stime > 23 || Stime < 0 || Etime < 0 || Etime > 23) Toast.makeText(getApplicationContext(), "시간은 0~23사이의 값을 입력해주세요!", Toast.LENGTH_SHORT).show();
+                    else {
+                        DbHandler3 dbHandler = new DbHandler3(TabActivity_3.this);
+                        dbHandler.insertUserDetails(date,startTime,endTime,name);
+                        Toast.makeText(getApplicationContext(), "저장", Toast.LENGTH_SHORT).show();
+                        intent = new Intent(TabActivity_3.this,DetailsActivity.class);
+                        startActivity(intent);
+                    }
                 }
-                else {
-                    DbHandler3 dbHandler = new DbHandler3(TabActivity_3.this);
-                    dbHandler.insertUserDetails(date,startTime,endTime,name,color);
-                    Toast.makeText(getApplicationContext(), "저장", Toast.LENGTH_SHORT).show();
+                catch (NumberFormatException e) {
+                    Toast.makeText(getApplicationContext(), "날짜, 시간에는 숫자만 입력해주세요!", Toast.LENGTH_SHORT).show();
                 }
-                intent = new Intent(TabActivity_3.this,DetailsActivity.class);
-                startActivity(intent);
             }
         });
     }
